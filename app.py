b52cf496e066cb74e5a8d80cd1f55c12d94e48f2
Jan Marthedal Rasmussen <jan@janmr.com>
2026-01-02 14:21:55 +0100

    Simplify

--- app.py
+++ app.py
@@ -9,9 +9,7 @@ app = Flask(__name__)
 
 # Load ephemeris data
 ts = load.timescale()
-print("Load bsp")
 eph = load('de421.bsp')
-print("Done loading bsp")
 
 @app.route('/')
 def hello_world():
@@ -99,73 +97,31 @@ def sun():
         f = almanac.dark_twilight_day(eph, location)
         times, events = almanac.find_discrete(t0, t1, f)
 
-        print(times)
-        print(events)
-
         # Map event codes to twilight phases
         # 0 = Dark (night, sun below -18°)
         # 1 = Astronomical twilight (sun between -18° and -12°)
         # 2 = Nautical twilight (sun between -12° and -6°)
         # 3 = Civil twilight (sun between -6° and -0.833°)
         # 4 = Day (sun above -0.833°)
+        PHASE_TO_KEY = {
+            1: 'astronomical',
+            2: 'nautical',
+            3: 'civil',
+            4: 'full',
+        }
 
-        # Initialize all times as None
-        astro_up = None
-        nautical_up = None
-        civil_up = None
-        full_up = None
-        full_down = None
-        civil_down = None
-        nautical_down = None
-        astro_down = None
+        prev_phase = 0
+        sunrise = {}
+        sunset = {}
 
         # Process events to extract twilight times
-        for ti, event in zip(times, events):
-            time_str = ti.astimezone(tz).strftime('%Y-%m-%dT%H:%M:%S%z')
-
-            if event == 1:  # Entering astronomical twilight from dark
-                astro_up = time_str
-            elif event == 2:  # Entering nautical twilight
-                nautical_up = time_str
-            elif event == 3:  # Entering civil twilight
-                civil_up = time_str
-            elif event == 4:  # Entering day (sunrise)
-                full_up = time_str
-            # Reverse transitions (going back down)
-            # When transitioning from higher to lower number, it's a "down" event
-
-        # For sunset events, we need to check transitions in reverse
-        # Process again looking for descending transitions
-        for i in range(len(events) - 1):
-            if events[i] == 4 and events[i + 1] == 3:  # Day to civil twilight (sunset)
-                full_down = times[i + 1].astimezone(tz).strftime('%Y-%m-%dT%H:%M:%S%z')
-            elif events[i] == 3 and events[i + 1] == 2:  # Civil to nautical
-                civil_down = times[i + 1].astimezone(tz).strftime('%Y-%m-%dT%H:%M:%S%z')
-            elif events[i] == 2 and events[i + 1] == 1:  # Nautical to astronomical
-                nautical_down = times[i + 1].astimezone(tz).strftime('%Y-%m-%dT%H:%M:%S%z')
-            elif events[i] == 1 and events[i + 1] == 0:  # Astronomical to dark
-                astro_down = times[i + 1].astimezone(tz).strftime('%Y-%m-%dT%H:%M:%S%z')
-
-        # Build response
-        sunrise = {}
-        if astro_up:
-            sunrise['astro'] = astro_up
-        if nautical_up:
-            sunrise['nautical'] = nautical_up
-        if civil_up:
-            sunrise['civil'] = civil_up
-        if full_up:
-            sunrise['full'] = full_up
-
-        sunset = {}
-        if full_down:
-            sunset['full'] = full_down
-        if civil_down:
-            sunset['civil'] = civil_down
-        if nautical_down:
-            sunset['nautical'] = nautical_down
-        if astro_down:
-            sunset['astro'] = astro_down
+        for ti, phase in zip(times, events):
+            time_str = ti.astimezone(tz).strftime('%Y-%m-%dT%H:%M:%S')
+            if phase > prev_phase:
+                sunrise[PHASE_TO_KEY[phase]] = time_str
+            else:
+                sunset[PHASE_TO_KEY[prev_phase]] = time_str
+            prev_phase = phase
 
         result = {
             'timezone': str(tz),
